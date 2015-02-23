1ad162a5c33737d165b5dde03cc118f0d577fc99
Andreo Vieira <andreoav@gmail.com>
2015-02-23 11:31:19 -0300

    Default configuration key-values.

--- src/config/guardian.php
+++ src/config/guardian.php
@@ -1,7 +1,44 @@
 <?php
 
+/**
+ * Guardian - Laravel 5 ACL Package
+ * Author: PHPArtisans
+ */
 return [
 
+    /*
+     * Default Role model used by Guardian.
+     */
+    'role_model' => 'Artisans\Guardian\Role',
 
+    /*
+     * Default Permission model used by Guardian.
+     */
+    'permission_model' => 'Artisans\Guardian\Permission',
+
+    /*
+     * Roles table name
+     */
+    'roles_table' => 'roles',
+
+    /*
+     * Permissions table name
+     */
+    'permissions_table' => 'permissions',
+
+    /*
+     * Pivot table for roles and users
+     */
+    'role_user_table' => 'role_user',
+
+    /*
+     * Pivot table for permissions and roles
+     */
+    'permission_role_table' => 'permission_role',
+
+    /*
+     * Pivot table for permissions and users
+     */
+    'permission_user_table' => 'permission_user'
 
 ];
