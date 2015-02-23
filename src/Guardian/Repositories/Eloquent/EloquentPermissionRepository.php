f5d851e483e544c2655ff75cbcc8b7a130f1a9ef
Andreo Vieira <andreoav@gmail.com>
2015-02-22 23:30:25 -0300

    fix bindings 2

--- src/Guardian/Repositories/Eloquent/EloquentPermissionRepository.php
+++ src/Guardian/Repositories/Eloquent/EloquentPermissionRepository.php
@@ -1,7 +1,13 @@
 <?php namespace Artisans\Guardian\Repositories\Eloquent;
 
+use Artisans\Guardian\Permission;
 use Artisans\Guardian\Repositories\PermissionRepository;
 
 class EloquentPermissionRepository extends AbstractEloquentRepository implements PermissionRepository {
+
+	public function __construct(Permission $model)
+	{
+		parent::__construct($model);
+	}
 
 }
--- src/Guardian/Repositories/Eloquent/EloquentRoleRepository.php
+++ src/Guardian/Repositories/Eloquent/EloquentRoleRepository.php
@@ -1,8 +1,14 @@
 <?php namespace Artisans\Guardian\Repositories\Eloquent;
 
+use Artisans\Guardian\Role;
 use Artisans\Guardian\Repositories\RoleRepository;
 
 class EloquentRoleRepository extends AbstractEloquentRepository implements RoleRepository {
+
+	public function __construct(Role $model)
+	{
+		parent::__construct($model);
+	}
 
 	public function findByName($roleName)
 	{
