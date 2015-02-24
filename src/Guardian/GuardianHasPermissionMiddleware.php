ed3da7e64f10d68f2d245ca4ec773a45c8006dce
Andreo Vieira <andreoav@gmail.com>
2015-02-24 17:26:46 -0300

    Initial middleware implementation.

--- src/Guardian/GuardianHasPermissionMiddleware.php
+++ src/Guardian/GuardianHasPermissionMiddleware.php
@@ -23,13 +23,36 @@ class GuardianHasPermissionMiddleware {
 	}
 
 	/**
-	 * @param $request
+	 * @param \Illuminate\Contracts\Http\Request $request
 	 * @param callable $next
 	 * @return mixed
 	 */
 	public function handle($request, Closure $next)
 	{
-		return $next($request);
+		//TODO: find a way to get the permission name
+		$permission = $this->getPermission($request);
+
+		if ($permission)
+		{
+			if ($this->auth->user()->can($permission))
+			{
+				return $next($request);
+			}
+		}
+
+		return response('Forbidden', 403);
+		//return $next($request);
+	}
+
+	/**
+	 * @param \Illuminate\Contracts\Http\Request $request
+	 * @return mixed
+	 */
+	private function getPermission($request)
+	{
+		$routeActions = $request->getRoute()->getActions();
+
+		return array_get($routeActions, 'permission', false);
 	}
 	
 }
