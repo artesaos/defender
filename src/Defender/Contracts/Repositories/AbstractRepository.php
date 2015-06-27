47c3e19bb040936ba42d83331f5ac8c5c9a675c7
Andreo Vieira <andreoav@gmail.com>
2015-06-27 14:15:31 -0300

    Added `all` and `paginate` methods to the abstract repository.

--- src/Defender/Contracts/Repositories/AbstractRepository.php
+++ src/Defender/Contracts/Repositories/AbstractRepository.php
@@ -4,9 +4,15 @@ namespace Artesaos\Defender\Contracts\Repositories;
 
 interface AbstractRepository
 {
+    public function all();
+
+    public function paginate($perPage = 10);
+
     public function findById($id);
 
     public function findByName($name);
 
     public function getList($value, $key = 'id');
+
+    public function make(array $with = []);
 }
