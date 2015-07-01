5d5362ec61d1c39bfe275cd937bef048b845a45f
Vagner do Carmo <vluzrmos@gmail.com>
2015-07-01 18:26:48 -0300

    fix loading helpers

--- src/Defender/Providers/DefenderServiceProvider.php
+++ src/Defender/Providers/DefenderServiceProvider.php
@@ -142,6 +142,6 @@ class DefenderServiceProvider extends ServiceProvider
             return;
         }
 
-        require_once __DIR__."../helpers.php";
+        require_once __DIR__.'/../helpers.php';
     }
 }
