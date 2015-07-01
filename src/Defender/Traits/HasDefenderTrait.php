d2ddd1ffbcfd67fc3eefdb1c91f6cea3088d1eba
Vinicius Reis <luiz.vinicius73@gmail.com>
2015-07-01 12:30:37 -0300

    get all permissions from user

--- src/Defender/Traits/HasDefenderTrait.php
+++ src/Defender/Traits/HasDefenderTrait.php
@@ -2,10 +2,44 @@
 
 namespace Artesaos\Defender\Traits;
 
+use Illuminate\Support\Collection;
+
 /**
  * Class HasDefenderTrait.
  */
 trait HasDefenderTrait
 {
     use HasUserRolesTrait, HasUserPermissionsTrait;
+
+
+    /**
+     * Retrieve all user permissions
+     *
+     * @return Collection
+     */
+    public function getPermissions()
+    {
+
+        $roles = $this->roles()->get()->lists('id')->toArray();
+
+        $permissionsRoles = app('defender.permission')->getByRoles($roles)->toBase();
+
+        $permissions =  $this->permissions()->get()->toBase()->merge($permissionsRoles);
+
+        return $this->preparePermissions($permissions);
+    }
+
+    /**
+     * @param Collection $permissions
+     *
+     * @return Collection
+     */
+    private function preparePermissions(Collection $permissions){
+        return $permissions->map(function($perm){
+
+            unset($perm->pivot, $perm->created_at, $perm->updated_at);
+
+            return $perm;
+        });
+    }
 }
