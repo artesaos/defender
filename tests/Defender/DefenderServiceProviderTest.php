2da2e52fdef709df5e1de44b6876e74c03f752d8
Vagner do Carmo <vluzrmos@gmail.com>
2015-07-07 15:09:06 -0300

    testing blade directives

--- tests/Defender/DefenderServiceProviderTest.php
+++ tests/Defender/DefenderServiceProviderTest.php
@@ -10,6 +10,13 @@ use Illuminate\Support\Facades\Blade;
  */
 class DefenderServiceProviderTest extends AbstractTestCase
 {
+    /**
+     * TestCases that should not register the service provider.
+     * @var array
+     */
+    protected $dotNotRegisterForThatTestCases = [
+        'testShouldNotCompileDefenderTemplateHelpers',
+    ];
 
     /**
      * Verify if all services are in service container.
@@ -50,7 +57,7 @@ class DefenderServiceProviderTest extends AbstractTestCase
     /**
      * Verify if blade is rendering defender directives.
      */
-    public function testDefenderBladeDirectives()
+    public function testShouldCompileDefenderTemplateHelpers()
     {
         $view = __DIR__.'/stubs/view_with_defender.blade.txt';
         $expected = __DIR__.'/stubs/view_with_defender.blade.output.txt';
@@ -68,10 +75,39 @@ class DefenderServiceProviderTest extends AbstractTestCase
     }
 
     /**
+     * If configuration is disabled, template helpers will not be available.
+     * Note: The service provider should not be register before that test.
+     */
+    public function testShouldNotCompileDefenderTemplateHelpers()
+    {
+        $this->app['config']->set('defender.template_helpers', false);
+
+        $this->app->register('Artesaos\Defender\Providers\DefenderServiceProvider');
+
+        $view = __DIR__.'/stubs/view_with_defender.blade.txt';
+        $expected = __DIR__.'/stubs/view_with_defender.blade.output.txt';
+
+        $compiled = Blade::compileString(file_get_contents($view));
+
+        $this->assertNotEmpty($compiled);
+
+        $this->assertContains('@can', $compiled);
+        $this->assertContains('@is', $compiled);
+        $this->assertContains('@endcan', $compiled);
+        $this->assertContains('@endis', $compiled);
+
+        $this->assertStringNotEqualsFile($expected, $compiled);
+    }
+
+    /**
      * {@inheritdoc}
      */
     protected function getPackageProviders($app)
     {
+        if (in_array($this->getName(), $this->dotNotRegisterForThatTestCases)) {
+            return [];
+        }
+
         return [
             'Artesaos\Defender\Providers\DefenderServiceProvider',
         ];
