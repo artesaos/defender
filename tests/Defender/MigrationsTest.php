0b4b9e690c442ca61acd3c0e7f7311f2fc559fb7
Vagner do Carmo <vluzrmos@gmail.com>
2015-07-07 23:32:02 -0300

    Asserting columns on each table migrated.

--- tests/Defender/MigrationsTest.php
+++ tests/Defender/MigrationsTest.php
@@ -34,7 +34,7 @@ class MigrationsTest extends AbstractTestCase
     }
 
     /**
-     *
+     * Should all tables exists.
      */
     public function testShouldSeeTablesOnDatabase()
     {
@@ -42,24 +42,90 @@ class MigrationsTest extends AbstractTestCase
         $schema = $this->app['db']->connection()->getSchemaBuilder();
 
         $tables = [
-            config('auth.table', 'users'),
-            config('defender.role_table', 'roles'),
-            config('defender.permission_table', 'permissions'),
-            config('defender.permission_user_table', 'permission_user'),
-            config('defender.permission_role_table', 'permission_role'),
-            config('defender.role_user_table', 'role_user'),
+            /*
+             * Users Table.
+             */
+            config('auth.table', 'users') => [
+                'id',
+                'name',
+                'email',
+                'created_at',
+                'updated_at',
+            ],
+
+            /*
+             * Roles Table.
+             */
+            config('defender.role_table', 'roles') => [
+                'id',
+                'name',
+                'created_at',
+                'updated_at',
+            ],
+
+            /*
+             * Permissions Table.
+             */
+            config('defender.permission_table', 'permissions') => [
+                'id',
+                'name',
+                'readable_name',
+                'created_at',
+                'updated_at',
+            ],
+
+            /*
+             * Permissions User Relational Table.
+             */
+            config('defender.permission_user_table', 'permission_user') => [
+                'user_id',
+                config('defender.permission_key', 'permission_id'),
+                'value',
+                'expires',
+            ],
+
+            /*
+             * Permissoins Role Relational Table.
+             */
+            config('defender.permission_role_table', 'permission_role') => [
+                config('defender.permission_key', 'permission_id'),
+                config('defender.role_key', 'role_id'),
+                'value',
+                'expires',
+            ],
+
+            /*
+             * Roles Users Relational Table.
+             */
+            config('defender.role_user_table', 'role_user') => [
+                'user_id',
+                config('defender.role_key', 'role_id'),
+            ],
         ];
 
-        foreach ($tables as $table) {
+        foreach ($tables as $table => $columns) {
             $this->assertTrue($schema->hasTable($table), sprintf(
-                'Table \'%s\' not found in database',
+                'Table \'%s\' not found in database.',
                 $table
             ));
+
+            /*
+             * Intentional foreach.
+             * This could be replaced by:
+             * $this->assertTrue($schema->hasColumns($table, $columns)).
+             */
+            foreach ($columns as $col) {
+                $this->assertTrue(
+                    $schema->hasColumn($table, $col),
+                    sprintf('Table \'%s\' has no column \'%s\' in database.', $table, $col)
+                );
+            }
         }
     }
 
     /**
-     * Package service provider
+     * Package service provider.
+     * @param \Illuminate\Contracts\Foundation\Application $app
      * @return array
      */
     public function getPackageProviders($app)
