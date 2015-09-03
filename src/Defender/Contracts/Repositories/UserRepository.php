0d1cd5177267f75de430ea82bfd54a5d24f5c3fd
Andreo Vieira <andreoav@gmail.com>
2015-09-03 10:28:10 -0300

    Fix PSR-2

--- src/Defender/Contracts/Repositories/UserRepository.php
+++ src/Defender/Contracts/Repositories/UserRepository.php
@@ -1,10 +1,10 @@
 <?php
-	
+
 namespace Artesaos\Defender\Contracts\Repositories;
 
 interface UserRepository extends AbstractRepository
 {
-	public function attachRole($roleName);
-	
-	public function attachPermission($permissionName, array $options = []);
+    public function attachRole($roleName);
+    
+    public function attachPermission($permissionName, array $options = []);
 }
