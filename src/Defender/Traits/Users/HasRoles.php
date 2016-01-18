40df859c378a129d039412cb1a3703e513e386c6
Tiago Perrelli <tiagoyg@gmail.com>
2016-01-18 10:27:24 -0300

    bugfix - revert 1st to change

--- src/Defender/Traits/Users/HasRoles.php
+++ src/Defender/Traits/Users/HasRoles.php
@@ -37,7 +37,7 @@ trait HasRoles
     public function hasRole($role)
     {
         return $this->roles
-            ->where('name', is_object($role) ? $role->name : $role)
+            ->where('name', $role)
             ->first() != null;
     }
 
@@ -48,7 +48,7 @@ trait HasRoles
      */
     public function attachRole($role)
     {
-        if (! $this->hasRole($role)) {
+        if (! $this->hasRole($role->name)) {
             $this->roles()->attach($role);
         }
     }
