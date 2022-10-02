bb937ac6f3945f0de2f347b1b3ed968281dd0826
mstfkhazaal <47450090+mstfkhazaal@users.noreply.github.com>
2022-10-02 13:25:09 +0300

    Update EloquentPermissionRepository.php

--- src/Defender/Repositories/Eloquent/EloquentPermissionRepository.php
+++ src/Defender/Repositories/Eloquent/EloquentPermissionRepository.php
@@ -7,7 +7,7 @@ use Artesaos\Defender\Contracts\Repositories\PermissionRepository;
 use Artesaos\Defender\Exceptions\PermissionExistsException;
 use Carbon\Carbon;
 use Illuminate\Contracts\Foundation\Application;
-
+use Illuminate\Support\Facades\DB;
 /**
  * Class EloquentPermissionRepository.
  */
@@ -72,7 +72,9 @@ class EloquentPermissionRepository extends AbstractEloquentRepository implements
     public function getActivesByUser($user)
     {
         $table = $user->permissions()->getTable();
-
+        if ($user->isSuperUser()) {
+            return DB::table(config('defender.permission_table', 'permissions'))->get();
+        }
         return $user->permissions()
             ->where($table.'.value', true)
             ->where(function ($q) use ($table) {
